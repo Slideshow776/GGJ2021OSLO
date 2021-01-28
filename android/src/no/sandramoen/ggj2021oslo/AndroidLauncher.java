d9cd9c936152740486cb1373264ba20acb848beb
Sandra Moen <sandramoen01@gmail.com>
2021-01-28 15:51:05 +0100

    added libGDX

---
+++ android/src/no/sandramoen/ggj2021oslo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.sandramoen.ggj2021oslo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.sandramoen.ggj2021oslo.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/no/sandramoen/ggj2021oslo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.sandramoen.ggj2021oslo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.sandramoen.ggj2021oslo.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
